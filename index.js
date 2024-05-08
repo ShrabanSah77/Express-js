a190e5f39d46482dd17bd718818dfbbdea681c1d
ShrabanSah77 <shrabanshah77@gmail.com>
2024-05-08 16:26:06 +0545

    updated code

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
 require("dotenv").config();
-
 const express = require("express");
+
+const indexRouter = require("./routes");
+
 const app = express();
 const PORT = Number(process.env.PORT);
 
 app.use(express.json());
 
-app.get("/id:", (req, res) => {
-  res.json({ msg: "Get Hello world by Shraban" });
-});
-
-app.post("/id:", (req, res) => {
-  // Client send the data
-  console.log({ query: req.query, params: req.params, body: req.body });
-  res.json({ msg: "Post Hello world by Shraban" });
-});
-
-app.put("/id:", (req, res) => {
-  // Client send the data
-  res.json({ msg: "Put Hello world by Shraban" });
-});
-
-app.patch("/", (req, res) => {
-  // Client send the data
-  res.json({ msg: "Patch Hello world by Shraban" });
-});
-
-app.delete("/", (req, res) => {
-  res.json({ msg: "Delete Hello world by Shraban" });
-});
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Application is running on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,32 @@ const router = express.Router();
 
 const movieRouter = require("../modules/movies/movie.api");
 
-router.get("/api/v1", (req, res, next) => {
-  try {
-    res.json({ msg: "MovieMate API is working..." });
-  } catch (e) {
-    next(e);
-  }
+router.get("/id:", (req, res) => {
+  res.json({ msg: "Get Hello world by Shraban" });
+});
+
+router.post("/id:", (req, res) => {
+  // Client send the data
+  console.log({ query: req.query, params: req.params, body: req.body });
+  res.json({ msg: "Post Hello world by Shraban" });
+});
+
+router.put("/id:", (req, res) => {
+  // Client send the data
+  res.json({ msg: "Put Hello world by Shraban" });
+});
+
+router.patch("/", (req, res) => {
+  // Client send the data
+  res.json({ msg: "Patch Hello world by Shraban" });
+});
+
+router.delete("/", (req, res) => {
+  res.json({ msg: "Delete Hello world by Shraban" });
+});
+
+router.listen(PORT, () => {
+  console.log(`Application is running on port ${PORT}`);
 });
 
 router.use("/api/v1/movies", movieRouter);
