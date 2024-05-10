83f976739eb438bb3288b7f05a415ee759447883
ShrabanSah77 <shrabanshah77@gmail.com>
2024-05-10 14:25:02 +0545

    modify

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(express.json());
 // I am the routing mechanism, I will send the API request from / to user index
 app.use("/", indexRouter);
 
+// Error handling
+
+app.use((err, req, res, next) => {
+  const errorMsg = err ? err.toString() : "Something went wrong";
+  res.status(500).json({ msg: errorMsg });
+});
+
 app.listen(PORT, () => {
   console.log("Application is running on port 8000");
 });
--- routes/catchError.js
+++ routes/catchError.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/register", (req, res, next) => {
+  res.json({ msg: "register user" });
+});
+
 // login scenario
 
-router.post("/", (req, res) => {
-  
-    
+router.post("/login", (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log({ email, password });
+    // logic
+    if (email != "shrabanshah77@gmail.com" && password != "123") {
+      throw new Error("Invalid credentials");
+    }
+    res.json({ msg: "login user" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
