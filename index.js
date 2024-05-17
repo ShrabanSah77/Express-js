4ebabfdeb1440e184900c2ddf39d2dd4454bd73f
ShrabanSah77 <shrabanshah77@gmail.com>
2024-05-17 14:38:32 +0545

    update module

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+
 const indexRouter = require("./routes");
+
+// .then and .catch use in promise
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Database connected successfully..");
+  })
+  .catch((e) => {
+    console.log("Database Error", e);
+  });
 
 const app = express();
 const PORT = Number(process.env.PORT);
