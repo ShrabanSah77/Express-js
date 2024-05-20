37c64c098d5fd010d3f806b5ec49e03d7200b5a7
ShrabanSah77 <shrabanshah77@gmail.com>
2024-05-20 15:49:37 +0545

    Updated tokens and secure

--- utils/secure.js
+++ utils/secure.js
@@ -1,7 +1,8 @@
 const { checkRole, verifyToken } = require("./token");
+const userModel = require("../modules/users/user.model");
 
-const secure = (sysRole) => {
-  return (req, res, next) => {
+const secure = (sysRole = []) => {
+  return async (req, res, next) => {
     try {
       const { access_token } = req.headers;
       // what to do if no token
@@ -11,10 +12,20 @@ const secure = (sysRole) => {
       // token expired??
       if (!isValid) throw new Error("Token expired");
       res.json({ isValid });
-      const { data } = isValid; // const data = isValid.data
+      const { data } = isValid;
+      // Check user email with database
+      const userInfo = await userModel.findOne({
+        email: data?.email,
+        isActive: true,
+        isEmailVarified: true,
+      });
+      if (!userInfo) throw new Error("User not found");
+      // const data = isValid.data
       // RBAC (Role-Based Access Control)
-      const validRole = checkRole({ sysRole, userRole: data?.Roles || [] });
+      // RBAC VS PBAC VS ABAC
+      const validRole = checkRole({ sysRole, userRole: userInfo?.Roles || [] });
       if (!validRole) throw new Error("User unauthorized");
+      req.currentUser = userInfo?._id;
       next();
     } catch (e) {
       next(e);
--- utils/token.js
+++ utils/token.js
@@ -15,12 +15,10 @@ const generateToken = (payload) =>
 const verifyToken = (token) => JWT.verify(token, process.env.JWT_SECRET);
 
 const checkRole = ({ sysRole, userRole }) =>
-  userRole.some((role) => sysRole.includes(role));
+  sysRole.length === 0 ? true : userRole.some((role) => sysRole.includes(role));
 
 const generateOtp = () => {
   return Crypto.randomInt(100000, 999999);
 };
-
-console.log(generateOtp());
 
 module.exports = { checkRole, generateToken, verifyToken, generateOtp };
